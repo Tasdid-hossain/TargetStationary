15e3d9b8b384ec0033ce57e5fe29e381349c378a
Tasdid-hossain <101210541@students.swinburne.edu.my>
2019-10-14 19:37:06 +0800

    search function on product page

--- app/src/main/java/com/example/targetstationary/ProductActivity.java
+++ app/src/main/java/com/example/targetstationary/ProductActivity.java
@@ -8,6 +8,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -25,10 +27,17 @@ import com.example.targetstationary.ViewHolder.ProductViewHolder;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.mancj.materialsearchbar.MaterialSearchBar;
 import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProductActivity extends AppCompatActivity {
 
@@ -40,9 +49,15 @@ public class ProductActivity extends AppCompatActivity {
 
     String CategoryID;
 
+
     RecyclerView recycler_product;
     RecyclerView.LayoutManager layoutManager;
     FirebaseRecyclerAdapter<ProductModel, ProductViewHolder> adapterProduct;
+
+    //search functionality
+    FirebaseRecyclerAdapter<ProductModel, ProductViewHolder> searchAdapter;
+    List<String> suggestList = new ArrayList<>();
+    MaterialSearchBar materialSearchBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,6 +86,114 @@ public class ProductActivity extends AppCompatActivity {
         }
 
         recycler_product.setAdapter(adapterProduct);
+
+        /*Search functions*/
+        materialSearchBar = (MaterialSearchBar) findViewById(R.id.searchBar);
+        materialSearchBar.setHint("Enter your product name");
+        loadSuggested();
+        materialSearchBar.setLastSuggestions(suggestList);
+        materialSearchBar.setCardViewElevation(10);
+        materialSearchBar.addTextChangeListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                /*Suggest list will change according to the text input from users*/
+                List<String> suggest = new ArrayList<String>();
+                for(String search:suggestList)
+                {
+                    if(search.toLowerCase().contains(materialSearchBar.getText().toLowerCase()))
+                        suggest.add(search);
+                }
+                materialSearchBar.setLastSuggestions(suggest);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        materialSearchBar.setOnSearchActionListener(new MaterialSearchBar.OnSearchActionListener() {
+            @Override
+            public void onSearchStateChanged(boolean enabled) {
+                //When search bar is close restore to original
+                if(!enabled)
+                    recycler_product.setAdapter(adapterProduct);
+            }
+
+            @Override
+            public void onSearchConfirmed(CharSequence text) {
+                //when search finish
+                //show result
+                startSearch(text);
+
+            }
+
+            @Override
+            public void onButtonClicked(int buttonCode) {
+
+            }
+        });
+    }
+
+    private void startSearch(CharSequence text) {
+        /*This is basically a query that will be included when creating the search adapter*/
+        FirebaseRecyclerOptions searchOptions = new FirebaseRecyclerOptions.Builder<ProductModel>().
+                setQuery(productQuery.orderByChild("Name").startAt(text.toString()).
+                                endAt(text.toString()+"\uf8ff"),
+                ProductModel.class).build();
+
+        searchAdapter = new FirebaseRecyclerAdapter<ProductModel, ProductViewHolder>(searchOptions) {
+            @Override
+            protected void onBindViewHolder(@NonNull ProductViewHolder holder, int position, @NonNull ProductModel model) {
+                holder.textProductName.setText(model.getName());
+                Picasso.get().load(model.getImage()).into(holder.imageViewProduct);
+                holder.textProductPrice.setText(model.getPrice());
+
+                final ProductModel clickItem = model;
+                holder.setItemClickListener(new ItemClickListener() {
+                    @Override
+                    public void onClick(View view, int position, boolean isLongClick) {
+                        Toast.makeText(ProductActivity.this, ""+clickItem.getName(), Toast.LENGTH_SHORT).show();
+
+                        /*Goes into Product Details*/
+                        Intent prodDetails = new Intent(ProductActivity.this, ProductDetails.class);
+                        prodDetails.putExtra("ProductID", adapterProduct.getRef(position).getKey());
+                        startActivity(prodDetails);
+                    }
+                });
+            }
+
+            @NonNull
+            @Override
+            public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.product_item, parent, false);
+                return new ProductViewHolder(view);
+            }
+        };
+        searchAdapter.startListening();
+        recycler_product.setAdapter(searchAdapter);
+    }
+
+    private void loadSuggested(){
+        product.orderByChild("CategoryID").equalTo(CategoryID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot postSnapShot:dataSnapshot.getChildren())
+                {
+                    ProductModel item = postSnapShot.getValue(ProductModel.class);
+                    suggestList.add(item.getName());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     private void loadProduct(String categoryID) {
@@ -117,12 +240,14 @@ public class ProductActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         adapterProduct.startListening();
+
     }
 
     @Override
     protected void onStop() {
         super.onStop();
         adapterProduct.stopListening();
+        searchAdapter.stopListening();
     }
 
     /*BottomNavigationView Setup*/
